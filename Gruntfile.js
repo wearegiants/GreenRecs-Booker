723b48451ba6c931a863c02875f8b35e66cbaf70
scneptune <rmalcome@gmail.com>
2014-06-23 21:52:52 -0700

    added a grunt file for tracking less files and compiling/minifying js. added some less styles to pickup where the calendar left off. cleaned up the the enqueuing includes.

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      options: {
+	   sourceMap: 'js/greensched.min.js.map',
+        sourceMappingURL: '/app/plugins/greenrecs-schedule/js/greensched.min.js.map'
+      },
+      dist: {
+        files: {
+		'js/greensched.min.js' : [
+			'js/*.js'
+		]	
+		}
+      }
+    },
+    less: {
+      dist: {
+        options: {
+          compress: true,
+          cleancss: true,
+          report: 'min'
+        },
+        files: {
+          'css/greensched.min.css': [
+            'less/*.less'
+          ]
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'less/*.less'
+        ],
+        tasks: ['less:dist']
+      },
+      js: {
+        files: [
+          'js/*.js',
+          'vendors/js/*.js'
+        ],
+        tasks: ['uglify']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: 2525,
+        },
+        files: [
+          
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'css/greensched.min.css',
+        'js/greensched.min.js'
+      ]
+    },
+  });
+
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  // Default task(s).
+    grunt.registerTask('default', [
+    'clean:dist',
+    'less:dist',
+    'uglify:dist',
+  ]);
+    grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
--- grBoot.php
+++ grBoot.php
@@ -9,15 +9,23 @@ Version: 0.0.1
 
 class grBoot {
 	function __construct() {
-			add_action('wp_enqueue_scripts', array($this, 'register_calendar_script'));
+		$this->callback_hooks();
 	}
-
-	public function register_calendar_script() {
-		wp_register_script( 'jquery_calendar', plugins_url( 'vendors/js/jquery.weekcalendar.js',  __FILE__  ) , array('jquery', 'jquery-ui-core', 'jquery-ui-widget', 'jquery-ui-mouse', 'jquery-ui-position', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-resizable', 'jquery-ui-selectable', 'jquery-ui-sortable', 'jquery-ui-dialog', 'jquery-ui-datepicker'));
+	function callback_hooks () {
+		add_action('wp_enqueue_scripts', array($this, 'register_calendar_script'));
+	}
+	function register_calendar_script() {
+		wp_register_script( 'jquery_calendar', plugins_url( 'vendors/js/jquery.weekcalendar.js',  __FILE__  ) , array('jquery', 'jquery-migrate', 'jquery-ui-core', 'jquery-ui-widget', 'jquery-ui-mouse', 'jquery-ui-position', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-resizable', 'jquery-ui-selectable', 'jquery-ui-sortable', 'jquery-ui-dialog', 'jquery-ui-datepicker'));
+		wp_register_script('date_lib', plugins_url( 'vendors/js/date.js', __FILE__ ), 'jquery');
 		wp_register_script('calendar_custom', plugins_url( 'js/Schedule-Runner.js', __FILE__), 'jquery' );
+		wp_enqueue_script('date_lib');
 		wp_enqueue_script('jquery_calendar' );
 		wp_enqueue_script( 'calendar_custom');
+		wp_enqueue_style('cal_add_greenrec', plugins_url( 'css/greensched.min.css', __FILE__ ) );
 		wp_enqueue_style('jquery_cal_style', plugins_url( 'vendors/css/jquery.weekcalendar.css',  __FILE__  ), 'jquery-ui-core' );
+		wp_enqueue_style('cal_add_style', plugins_url('vendors/css/default.css', __FILE__) );
+		//wp_enqueue_style('cal_add_gcal_style', plugins_url('vendors/css/gcalendar.css', __FILE__) );
+		wp_enqueue_style('jquery_ui_custom', plugins_url('vendors/css/jquery-ui-1.8.11.custom.css', __FILE__), 'jquery-ui-core');
 
 	}
 
@@ -26,3 +34,4 @@ class grBoot {
 		var_dump($post);
 	}
 }
+new grBoot();
