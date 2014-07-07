e110dcaba011676875bd2900cc5124c75f20cb6a
scneptune <rmalcome@gmail.com>
2014-07-07 07:47:20 -0700

    created an activation hook to use when the plugin is enabled and disabled. it fires off the page creation stuff within the controller.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Version: 0.0.1
 */
 
 //index.php
-
+if(defined("ABSPATH") && defined("WPINC")) {
   define("GR_PLUGIN_PATH", dirname(__FILE__));
   define("GR_PLUGIN_URL", plugin_dir_url(__FILE__));
   define("GR_URL_API", getenv("GR_URL_API"));
@@ -23,4 +23,13 @@ Version: 0.0.1
   include_once "yerbaverde/controllerVerde.php";
   include_once "yerbaverde/apiConnect.php";
 
+$yvApi = \YerbaVerde\controllerVerde::getInstance();
 
+  register_activation_hook(__FILE__,  function() use ($yvApi) {
+    $yvApi->doEnable();
+  });
+
+  register_deactivation_hook(__FILE__, function() use ($yvApi) {
+    $yvApi->doDisable();
+  });
+ }
