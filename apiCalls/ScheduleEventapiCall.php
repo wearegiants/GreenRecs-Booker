1fd014aad87e7421ab831a665b43d439cd3ea983
scneptune <rmalcome@gmail.com>
2014-07-24 04:49:52 -0700

    moved around some of the templating for the calendar. reformatted the param made key value pairs from the Post. also added the redirect to the ajax to make sure it comes.

--- apiCalls/ScheduleEventapiCall.php
+++ apiCalls/ScheduleEventapiCall.php
@@ -11,17 +11,34 @@ class ScheduleEventapiCall extends apiCall implements apiCallProperties {
   }
 
   function getTemplate() {
-    return false;
+    return include_once(GR_PLUGIN_PATH . '/templates/grCal.php');
   }
 
   function hasPage() {
-     return true;
+     return false;
   }
 
 
   function doSubmitProcess($params) {
-     
+    $errors = array();
+     if (!isset($params['event'])) {
+      $errors[] = array (
+            "message" => "You do not have an appointment. Please choose a time on the calendar. ",
+            "field" => '[event]'
+            );
+     }
+    $redirect = (isset($params['redirect_to']) ? $params['redirect_to'] : '');
+    parse_str($params['event'], $new_params);
 
+    $final_params = array (
+      'appointment_hash' => hash_hmac('sha256', mt_rand(0, 800), $this->getTheSalt()),
+      'appointment_start' => $new_params['start'],
+      'appointment_end' => $new_params['end'],
+      'doc_name' => $new_params['doc_name']
+      ); 
+    
+    setcookie('appointment_hash', $final_params['appointment_hash'], time()+3600);
+    
     if(count($errors) > 0) {
       return $this->echoJSONResponse(
         array(
@@ -32,15 +49,13 @@ class ScheduleEventapiCall extends apiCall implements apiCallProperties {
       );
     }
 
-    $api_result = $this->callYerbaVerde("patient_add", $params);
-
-// var_dump($api_result);
-
+    $api_result = $this->callYerbaVerde("eventPost", $final_params);
  $this->echoJSONResponse(
       array(
         "status" => 0,
         "message" => "Successfully submitted a schedule time",
-        "msgtype" => "global"
+        "msgtype" => "global",
+        "redirect" => $redirect
       )
     );
 
