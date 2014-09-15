9ffaa4fd3f662f16291b453a3c0d3d357e7795bb
scneptune <rmalcome@gmail.com>
2014-09-15 08:57:46 -0700

    Added templates and the api call, both could use a little more cleanup.

---
+++ apiCalls/VerifyapiCall.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace YerbaVerde;
+
+class VerifyapiCall extends apiCall implements apiCallProperties {
+
+  function getDefaultTitle() {
+    return "Verify Patient";
+  }
+
+  function getUniqueName() {
+    return "Verify";
+  }
+
+  function getTemplate() {
+    include_once(GR_PLUGIN_PATH . '/templates/verifyTempl.php');
+  }
+
+  function hasPage() {
+    return true;
+  }
+
+ function alphavalid($value, $fieldname) {
+            if (!preg_match('/^[a-z .\-]+$/i', $value)) {
+              return array(
+                "message" => "This field can only contain alphabetic characters. ie. (A-Z).",
+                "field" => $fieldname
+              );
+            }
+      }
+      function noempty($value, $fieldname) {
+          if( empty($value) ){
+            return $errors[] = array(
+              "message" => "This is a required field and must be filled out to the best of your knowledge",
+              "field" => $fieldname
+              );
+          } 
+      }
+
+  function doSubmitProcess($params) {
+      $errors = array();
+      $params['dob'] = $params['dob-year'] . '-' . $params['dob-month'] . '-' . $params['dob-day'];
+
+      
+    foreach( $params as $key => $value) {      
+      if ($key == 'approval_hash' && (strlen($value) !== 12)) {
+        $errors[] = array (
+            "message" => "Please recheck the value your entered in this field, there appears to be some missing characters",
+            "field" => $key
+            );
+      } else {
+        $this->noempty($value, $key);
+      }
+    }
+
+    if(count($errors) > 0) {
+      return $this->echoJSONResponse(
+        array(
+          "status" => 1,
+          "msgtype" => "bulk",
+          "errors" => $errors
+        )
+      );
+    }
+
+    $api_result = $this->callYerbaVerde("verify", $params);
+
+     $this->echoJSONResponse(
+          array(
+            "status" => 0,
+            "message" => "Successfully submitted an application",
+            "msgtype" => "global"
+          )
+        );
+
+  }
+}
---
+++ templates/verifyTempl.php
@@ -0,0 +1,107 @@
+
+	<form role="form" id="verifyPatient" action="<?php echo $this->getFormActionUrl(); ?>" method="POST" enctype="multipart/form-data" >
+		<?php echo $this->getSubmitFields('verify');?>
+	<div class="row" style="margin-bottom:30px;">
+	        <div class="col-md-12">
+	            <p>* = Required</p>
+	        </div>
+		<div class="col-md-6">
+			<div class="form-group form-group-lg">
+			<label for='data[approval_hash]'>Patient's Approval Number*</label>
+				<div class="input-group">
+					<div class="input-group-addon">#</div> 
+					<input type='text' name='data[approval_hash]' class='form-control' placeholder='eg. 4eh432ba' />
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="form-group form-group-lg">
+				<div class="col-md-4">
+			                <label class="col-md-12  col-xs-12" for="data[dob]">Date of Birth*</label>
+			                <div class="col-md-4 col-sm-4 col-xs-4">
+			                    <div class="input-group">
+			                        <div class="btn-group date">
+			                            <select name="data[dob-day]" data-error="data[dob-day]">
+			                            <?php 
+			                                for ($day = 01; $day < 32; $day++) {
+			                                    $selected = '';
+			                                    if ($day < 10) {
+			                                        if ($day == 01) {
+			                                            $selected = 'selected';
+			                                        }
+			                                        $day = 0 . $day;
+			                                    }
+			                                    echo "<option " . $selected ." value='" . $day . "'>" . $day . "</option>";
+
+			                                }
+			                            ?>
+			                            </select>
+			                        </div>
+			                        <!-- Button Group -->
+			                    </div>
+			                    <!-- /input-group -->
+			                </div>
+			                <!-- /.col-lg-3 -->
+
+			                <div class="col-md-4 col-sm-4 col-xs-4">
+			                    <div class="input-group">
+			                        <div class="btn-group date">
+			                            <select name="data[dob-month]" data-error="data[dob-month]">
+			                                <option selected value="01">January</option>
+			                                <option value="02">February</option>
+			                                <option value="03">March</option>
+			                                <option value="04">April</option>
+			                                <option value="05">May</option>
+			                                <option value="06">June</option>
+			                                <option value="07">July</option>
+			                                <option value="08">August</option>
+			                                <option value="09">September</option>
+			                                <option value="10">October</option>
+			                                <option value="11">November</option>
+			                                <option value="12">December</option>
+			                            </select>
+			                        </div>
+			                        <!-- Button Group -->
+			                    </div>
+			                    <!-- /input-group -->
+			                </div>
+			                <!-- /.col-lg-3 -->
+
+			                <div class="col-md-4 col-sm-4 col-xs-4">
+			                    <div class="input-group">
+			                        <div class="btn-group date">
+			                            <select name="data[dob-year]"  data-error="data[dob-year]">
+			                            <?php 
+			                            for ($iter = (date('Y') - 18); $iter > 1910; $iter--) {
+			                                $selected = '';
+			                                if ($iter == (date('Y') - 18)) {
+			                                    $selected ="selected";
+			                                }
+			                                 echo "<option ". $selected ." value='" .  $iter . "'>" . $iter . "</option>";
+			                                } 
+			                                ?>
+			                                
+			                            </select>
+			                        </div>
+			                        <!-- Button Group -->
+			                    </div>
+			                    <!-- /input-group -->
+			                </div>
+			                <!-- /.col-lg-3 -->
+
+
+			        </div>
+			        <!-- /.col-lg-3 -->
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-4">
+			<div class="form-group form-group-lg">
+				<input class="btn btn-primary btn-lg custom" data-form-id="verifyPatient" type="submit" value="Verify Patient" />
+			</div>
+		</div>
+	</div>
+
+	</form>
