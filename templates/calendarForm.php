46909863263e7b3e4b1945e1efaeb1d5f16eb810
scneptune <rmalcome@gmail.com>
2015-01-04 23:13:09 -0800

    appropriate form calls to the agreement page.

--- templates/calendarForm.php
+++ templates/calendarForm.php
@@ -1,7 +1,7 @@
 <section class="form-part" id="CalendarPicker">
     <form role="form" action="<?php echo $this->getFormActionUrl(); ?>" method="POST" enctype="multipart/form-data" class="show" id="calendarform">
     <?php echo $this->getSubmitFields('CalendarForm');?>
-    <input type="hidden" name="data[redirect_to]" value="<?php echo \YerbaVerde\controllerVerde::getPageUrl('agreement'); ?>" />
+    <input type="hidden" name="data[redirect_to]" value="<?php echo \YerbaVerde\controllerVerde::getPageUrl('AgreementForm'); ?>" />
         <div class="apptCalendar">
             <div id="calendar"></div>
         </div>
