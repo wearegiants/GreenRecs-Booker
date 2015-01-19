068f8acb6b3c6e997be5f69a44e78b310174077d
scneptune <rmalcome@gmail.com>
2015-01-19 09:53:48 -0800

    markup for calendar carousel.

--- templates/calendarForm.php
+++ templates/calendarForm.php
@@ -2,8 +2,23 @@
     <form role="form" action="<?php echo $this->getFormActionUrl(); ?>" method="POST" enctype="multipart/form-data" class="show" id="calendarform">
     <?php echo $this->getSubmitFields('CalendarForm');?>
     <input type="hidden" name="data[redirect_to]" value="<?php echo \YerbaVerde\controllerVerde::getPageUrl('AgreementForm'); ?>" />
-        <div class="apptCalendar">
-            <div id="calendar"></div>
+        <div id="apptCalendar">
+        	<div class="help-text">
+        		<h2> Select A Time That Works Best For You </h2>
+        	</div>
+            <div class="c">
+				<div class="c-list-container">
+					<ul class="c-list" id="scheduleList">
+					</ul>
+				</div>
+				<nav class="c-nav">
+					<a href="#" class="prev">&larr; Prev</a>
+					<a href="#" class="next">Next &rarr;</a>
+				</nav>
+			</div>
+        </div>
+        <div>
+        	<input type="submit" value="Request This Appointment" class="btn ">
         </div>
     </form>
 </section>
