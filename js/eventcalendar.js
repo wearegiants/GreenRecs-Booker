eb41d92eb7b28e74f99de0ab740eda22a1095a3b
scneptune <rmalcome@gmail.com>
2015-01-19 23:25:15 -0800

    got the carousel working, now to refine it

--- js/eventcalendar.js
+++ js/eventcalendar.js
@@ -19,106 +19,14 @@
             });
             window.events = data;
             GetDays();
-            
+            StartCarousel();
           } else {
             console.log(xhr.response)
           }
         };
         xhr.send();
 
-$pagedCarousel = {
-  resize : function () {
-   return window.onresize = function () {
-      sw = document.body.clientWidth,
-      breakpointSize = window.getComputedStyle(document.body,':after').getPropertyValue('content')
-    }  
-  },
-  init: function () {
-    return {
-    sw : document.body.clientWidth,
-    current: 0,
-    multiplier: 1,
-    breakpointSize: window.getComputedStyle(document.body,':after').getPropertyValue('content'),
-    $carousel: document.querySelectorAll('.c')[0],
-    $cList: document.getElementById('scheduleList'),
-    $cWidth: $cList.offsetWidth,
-    $li: document.querySelectorAll('.c li.day'),
-    $lilength: Array.prototype.slice.call($li),
-    numPages : ($lilength/multiplier),
-    $prev: document.querySelectorAll('.c-nav .prev')[0],
-    $next: document.querySelectorAll('.c-nav .next')[0]
-    }
-  }
-}
 
-        var sw = document.body.clientWidth,
-        current = 0,
-        breakpointSize = window.getComputedStyle(document.body,':after').getPropertyValue('content'),
-        multiplier = 1, /*Determines the number of panels*/
-        $carousel = $('.c'),
-        $cList = $('.c-list'),
-        $cWidth = $carousel.outerWidth(),
-        // $li = $('.c li.day'),
-        // $liLength = $li.size(),
-        $li = $('.c li.day'),
-        $lilength = $li.size();
-        console.log($lilength);
-        numPages = $liLength/multiplier,
-        $prev = document.querySelectorAll('.c-nav .prev')[0],
-        $next = document.querySelectorAll('.c-nav .next')[0];
-      
-      $(window).resize(function(){ //On Window Resize
-        sw = document.body.clientWidth;
-        $cWidth = $carousel.width();
-        breakpointSize = window.getComputedStyle(document.body,':after').getPropertyValue('content');  /* Conditional CSS http://adactio.com/journal/5429/ */
-        sizeCarousel();
-        posCarousel();
-      });
-      
-      function sizeCarousel() { //Determine the size and number of panels to reveal
-        current = 0;
-        if (breakpointSize == 'medium') {
-          multiplier = 2;      
-        } else if (breakpointSize == 'large') {
-          multiplier = 4;
-        } else {
-          multiplier = 1;
-        }
-        console.log(multiplier, breakpointSize, $lilength);
-
-        animLimit = $liLength/multiplier-1;
-        
-        $li.outerWidth($cWidth/multiplier); //Set panel widths
-        
-      }
-      
-      
-      function buildCarousel() { //Build the Carousel
-        sizeCarousel();
-      }
-
-      
-      function posCarousel() { //Animate Carousel. CSS transitions used for the actual animation.
-        var pos = -current * $cWidth;
-        console.log(pos);
-        $cList.css("left",pos);
-      }
-      
-      $prev.addEventListener('click', function(e){ //Previous Button on Click
-        e.preventDefault();
-        if(current>0) {
-          current--;
-        }
-        posCarousel();
-        
-      });
-      $next.addEventListener('click', function(e){ //Next Button Click
-        e.preventDefault();
-        if(current<animLimit) {
-          current++;
-        }
-        posCarousel();
-      });
 
       function GetDays() {
         days = Object.keys(events);
@@ -134,7 +42,6 @@ $pagedCarousel = {
           cSlideDay.appendChild(slotList);
           cListUl.appendChild(cSlideDay);
         });
-        buildCarousel();
         
       }
       function eventSlotMarkup(list) {
@@ -194,6 +101,78 @@ $pagedCarousel = {
               }
           }
       };
+
+      function StartCarousel () {
+            var sw = document.body.clientWidth,
+            current = 0,
+            breakpointSize = window.getComputedStyle(document.body,':after').getPropertyValue('content'),
+            multiplier = 1, /*Determines the number of panels*/
+            $carousel = $('.c'),
+            $cList = $('.c-list'),
+            $cWidth = $carousel.outerWidth(),
+            $li = $('.c li'),
+            $liLength = $li.size(),
+            numPages = $liLength/multiplier,
+            $prev = $('.c-nav .prev'),
+            $next = $('.c-nav .next');
+          
+          $(document).ready(function() {
+            buildCarousel();
+          });
+          
+          
+          $(window).resize(function(){ //On Window Resize
+            sw = document.body.clientWidth;
+            $cWidth = $carousel.width();
+            breakpointSize = window.getComputedStyle(document.body,':after').getPropertyValue('content');  /* Conditional CSS http://adactio.com/journal/5429/ */
+            sizeCarousel();
+            posCarousel();
+          });
+          
+          function sizeCarousel() { //Determine the size and number of panels to reveal
+            current = 0;
+            if (breakpointSize == 'medium') {
+              multiplier = 2;
+              
+            } else if (breakpointSize == 'large') {
+              multiplier = 3;
+            } else {
+              multiplier = 1;
+            }
+            
+            animLimit = $liLength/multiplier-1;
+            
+            $li.outerWidth($cWidth/multiplier); //Set panel widths
+            
+          }
+          
+          
+          function buildCarousel() { //Build the Carousel
+            sizeCarousel();
+          }
+          
+          function posCarousel() { //Animate Carousel. CSS transitions used for the actual animation.
+            var pos = -current * $cWidth;
+            $cList.css("left",pos);
+          }
+          
+          $prev.click(function(e){ //Previous Button Click
+            e.preventDefault();
+            if(current>0) {
+              current--;
+            }
+            posCarousel();
+            
+          });
+          $next.click(function(e){ //Next Button Click
+            e.preventDefault();
+            if(current<animLimit) {
+              current++;
+            }
+            posCarousel();
+          });
+
+      }
      
 
     })(this);
