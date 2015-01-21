0b8c93016015e8b271d9416ff8aeaacef5c1620d
scneptune <rmalcome@gmail.com>
2015-01-20 18:13:01 -0800

    added the confirmation modal.

--- templates/calendarForm.php
+++ templates/calendarForm.php
@@ -18,7 +18,24 @@
 			</div>
         </div>
         <div>
-        	<input type="submit" value="Request This Appointment" class="btn ">
+	        <div class="modal fade">
+			  <div class="modal-dialog">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <h4 class="modal-title">Confirm Date Selection</h4>
+			      </div>
+			      <div class="modal-body">
+			        <p id="appointment-date-seed"></p>
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			        <input type="submit" class="btn btn-success" value="Request This Appointment">
+			      </div>
+			    </div><!-- /.modal-content -->
+			  </div><!-- /.modal-dialog -->
+			</div><!-- /.modal -->
+        	
         </div>
     </form>
 </section>
