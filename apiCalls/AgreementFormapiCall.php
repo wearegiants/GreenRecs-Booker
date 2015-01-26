0422d8f389efd538b8ea8679fab086ad0d950026
scneptune <rmalcome@gmail.com>
2015-01-25 22:17:33 -0800

    make sure each field in this form are filled out.

--- apiCalls/AgreementFormapiCall.php
+++ apiCalls/AgreementFormapiCall.php
@@ -26,6 +26,22 @@ class AgreementFormapiCall extends apiCall implements apiCallProperties {
 
     $this->checkPID($params);
 
+
+    foreach($params as $key => $value) {
+      switch($key) {
+        case 'redirect_to':
+        case 'pid':
+        break;
+        default:
+        $checkEmptySig = $this->noempty($value, $key);
+        // var_dump($checkEmptySig); die();
+        if ($checkEmptySig) {
+          $errors[] = $checkEmptySig;
+        }
+        break;
+      }
+    } 
+
     if(count($errors) > 0) {
       return $this->echoJSONResponse(
         array(
@@ -40,12 +56,12 @@ class AgreementFormapiCall extends apiCall implements apiCallProperties {
     $redirect = (isset($params['redirect_to']) ? $params['redirect_to'] : '');
     unset($params['redirect_to']);
 
-    $api_result = $this->callYerbaVerde("patadd/agreement", $params);
+    // $api_result = $this->callYerbaVerde("patadd/agreement", $params);
 
  $this->echoJSONResponse(
       array(
         "status" => 0,
-        "message" => "Finished updating the symptom fields",
+        "message" => "Finished Agreement",
         "msgtype" => "global",
         "redirect" => $redirect,
         "session_hash" => hash_hmac('sha256', mt_rand(0, 100), $this->getTheSalt())
--- templates/agreementForm.php
+++ templates/agreementForm.php
@@ -1,7 +1,7 @@
 <section class="form-part" id="AgreementForm">
   <form role="form" action="<?php echo $this->getFormActionUrl(); ?>" method="POST" enctype="multipart/form-data" class="show" id="agreement">
     <?php echo $this->getSubmitFields('AgreementForm');?>
-    <input type="hidden" name="data[redirect_to]" value="" />
+    <input type="hidden" name="data[redirect_to]" value="<?php echo self::getPageUrl('confirmationPage'); ?>" />
     <div class="col-md-10 col-centered">
       <div class="row">
         
@@ -13,68 +13,68 @@
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign1]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign1]" data-error="data[sign1]"></div>
+          <div class="col-md-10"><label for="data[sign1]">
           I understand that I must be a California State resident to obtain an approval or recommendation for the use of cannabis (medical marijuana) under California’s Compassionate Use Act of 1996 (Health &amp; Safety Code #11362.5).
-          </p></div>
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign2]"></div>
-          <div class="col-md-10"><p>
-          I have provided an authentic, state or federal issued valid proof of my identity and/or California residency.
-          </p></div>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign2]" data-error="data[sign2]"></div>
+          <div class="col-md-10"><label for="data[sign2]">
+          I have provided an authentic, state or federal issued valid proof of my identity and/or California residency.
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign3]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign3]" data-error="data[sign3]"></div>
+          <div class="col-md-10"><label for="data[sign3]">
           I am 18 years of age or older.
-          </p></div>
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign4]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign4]" data-error="data[sign4]"></div>
+          <div class="col-md-10"><label for="data[sign4]">
           I acknowledge that GreenRecs or any and all doctors in the GreenRecs physicians network did
           has not directed, conspired, aid or assisted me (the patient) in obtaining medical marijuana.
-          </p></div>
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign5]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign5]" data-error="data[sign5]"></div>
+          <div class="col-md-10"><label for="data[sign5]">
           Physician Release of Liability - Agreed &amp; Accepted By Patient <br>
           <a href="#">Please read in full. Click here to view.</a>
-          </p></div>
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign6]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign6]" data-error="data[sign6]"></div>
+          <div class="col-md-10"><label for="data[sign6]">
           I have read and accept the GreenRecs terms and conditions in full.<br>
           <a href="#">Please read in full. Click here to view.</a>
-          </p></div>
+          </label></div>
           </div>
         </div></div>
         
         <div class="question col-md-12"><div class="row">
           <div class="input-group">
-          <div class="col-md-2"><input type="text" class="form-control" name="data[sign7]"></div>
-          <div class="col-md-10"><p>
+          <div class="col-md-2"><input type="text" class="form-control" name="data[sign7]" data-error="data[sign7]"></div>
+          <div class="col-md-10"><label for="data[sign7]">
           I acknowledge that the information provided is true and correct and under the penalty of perjury
           etc...etc... (final verbiage pending)
-          </p></div>
+          </label></div>
           </div>
         </div></div>
             
