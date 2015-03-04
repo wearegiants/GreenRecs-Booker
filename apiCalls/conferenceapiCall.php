aa12cedd8f4652908ee600fafe43527af558970d
scneptune <rmalcome@gmail.com>
2015-03-04 10:14:59 -0800

    corrected path for verifying appointments.

--- apiCalls/conferenceapiCall.php
+++ apiCalls/conferenceapiCall.php
@@ -21,38 +21,14 @@ class conferenceapiCall extends apiCall implements apiCallProperties {
   }
 
   function doSubmitProcess($params) {
-    $errors = array();
-    foreach ($params as $key => $value) {
 
-      if ($key == 'email'){
-        $testEmail = $this->existingInfo($value, $key, 'chkBasic'); 
-        if (!$testEmail) {
-          $errors[] = $testEmail;
-          }
-      }
-    }
+    $api_result = $this->callYerbaVerde("verify/appointment", $params);
 
-    if(count($errors) > 0) {
-      return $this->echoJSONResponse(
-        array(
-          "status" => 1,
-          "msgtype" => "bulk",
-          "errors" => $errors
-        )
-      );
-    }
-    
-    $redirect = (isset($params['redirect_to']) ? $params['redirect_to'] : '');
-    $params['dob'] = $params['dob-year'] . '-' . $params['dob-month'] . '-' . $params['dob-day'];
-    //unset this after we have iterated over it for the no empty check
-    unset($params['dob_day'], $params['dob_month'], $params['dob_year']);
-    
-    $api_result = $this->callYerbaVerde("reset/appointment", $params);
-
+    var_dump($api_result);
+    die();
       $this->echoJSONResponse(
             array(
               "status" => $api_result['status'],
-              "message" => "Successfully submitted a schedule time",
               "msgtype" => "global",
               'session_hash' => hash_hmac('sha256', mt_rand(0, 100), $this->getTheSalt()),
               "pid" => $api_result['id'],
