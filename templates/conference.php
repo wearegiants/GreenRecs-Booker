51f8ba585b642fb3c87f4a4dd19bf2e47c2702f5
scneptune <rmalcome@gmail.com>
2015-03-09 19:15:41 -0700

    no phi column, but there is a phi_id column.

--- templates/conference.php
+++ templates/conference.php
@@ -1,6 +1,10 @@
 <section>
-<?php echo var_dump($_GET); ?>
-
+<?php echo var_dump($_GET); 
+// $params = $this->getSubmitFields('conference');
+$params = [];
+$params['token']= $_GET['token']; ?>
+<?php $api_result = $this->callYerbaVerde("verify/appointment", $params); 
+var_dump($api_result); ?>
 
 <p> Hey Miles do your magic. </p>
 </section>
