03f5b1fdca5db5ec40d6effc02855c7d3fe68215
scneptune <rmalcome@gmail.com>
2015-03-14 13:48:53 -0700

    added the faked addt params to the apiCall.

--- templates/conference.php
+++ templates/conference.php
@@ -1,8 +1,11 @@
 <section>
-<?php echo var_dump($_GET); 
+<?php 
 // $params = $this->getSubmitFields('conference');
 $params = [];
-$params['token']= $_GET['token']; ?>
+$params['token']= $_GET['token'];
+$params['method'] = 'conference';
+$params['action'] = 'green_rec_form';
+$params['gr_wp_nonce'] = wp_create_nonce('green_rec_form_nonce'); ?>
 <?php $api_result = $this->callYerbaVerde("verify/appointment", $params); 
 var_dump($api_result); ?>
 
