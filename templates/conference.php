d92ff96049ace82f87656945fde3bf2db9c218b8
MilesPhllps <miles.phillips@gmail.com>
2015-03-15 21:33:38 -0400

    Updated Conference to connect to magnoliyan

--- templates/conference.php
+++ templates/conference.php
@@ -1,6 +1,38 @@
-<section>
-<?php echo var_dump($_GET); ?>
+<?php
 
+$params = array();
+$params['token'] = $_GET['token'];
+$res = $this->callYerbaVerde("verify/appointment", $params);
 
-<p> Hey Miles do your magic. </p>
-</section>
+echo "<pre>".var_export($res,true)."</pre>";
+$room = '';
+// Validate hash back to Yerbaverde with post
+if($res['status'] === -1) {
+	$room = $res['room'];
+	unset($res['status']);
+	unset($res['room']);	
+	setcookie('pat-info',base64_encode(json_encode($res)),time()+3600,'/');
+
+	
+	// do the magnolian stuff
+	?>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#mgVideoChat').mgVideoChat({
+        wsURL: 'ws://local.wordpress-trunk.dev:8080?room=<?php echo $room; ?>' //domain:port and room id info
+    });
+
+    //on connections change remove text message option
+    $('#mgVideoChat').mgVideoChat('on','connections',function(connections){
+        console.log('[mgVideoChat.connections]firedx',connections);
+        $(".chatMsg.btn").hide();
+		$(".cmdBtn.call").hide();
+    }); 
+});
+</script>
+
+<div id="mgVideoChat"></div>
+<?php
+}
+
+?>
