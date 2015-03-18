a1c6e147bc2fba17038d4407b9949a229f41218d
scneptune <rmalcome@gmail.com>
2015-03-17 21:48:47 -0700

    refined the conference with some nice boxes.

--- templates/conference.php
+++ templates/conference.php
@@ -8,23 +8,31 @@ $params['gr_wp_nonce'] = wp_create_nonce('green_rec_form_nonce'); ?>
 <?php 
 $res = $this->callYerbaVerde("verify/appointment", $params); 
 
-
-echo "<pre>".var_export($res,true)."</pre>";
 $room = '';
 // Validate hash back to Yerbaverde with post
-if($res['status'] === -1) {
+if(is_array($res)) {
 	$room = $res['room'];
 	unset($res['status']);
 	unset($res['room']);	
 	setcookie('pat-info',base64_encode(json_encode($res)),time()+3600,'/');
 
-	
+
 	// do the magnolian stuff
 	?>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4" style="padding-top: 40px; padding-bottom: 40px; height: 400px;">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h3>Hi, <?php echo ucwords($res['name']); ?>!</h3>
+					<p> This is temporary, but your token is valid and normally we would forward you from here on to the video conferencing suite.</p>
+				</div>
+			</div>
+	</div>
+</div>
 <script type="text/javascript">
 $(document).ready(function(){
     $('#mgVideoChat').mgVideoChat({
-        wsURL: 'ws://local.wordpress-trunk.dev:8080?room=<?php echo $room; ?>' //domain:port and room id info
+        wsURL: 'ws://<?php echo getenv('WP_HOME'); ?>:8080?room=<?php echo $room; ?>' //domain:port and room id info
     });
 
     //on connections change remove text message option
@@ -37,7 +45,18 @@ $(document).ready(function(){
 </script>
 
 <div id="mgVideoChat"></div>
-<?php
-}
-
+<?php 
+	} else {
+?>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4" style="padding-top: 40px; padding-bottom: 40px; height: 400px;">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<p>Whoops, looks like you're a little early for your appointment. Please, check back within 20 minutes of your appointment's start time</p>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php  
+	}
 ?>
